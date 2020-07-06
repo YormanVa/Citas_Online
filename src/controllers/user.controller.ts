9760e56883fe9f1e52ddc7400f381f84b7a40dd8
Jhonatan Toro Gomez <jhonatan.1701322237@ucaldas.edu.co>
2020-07-06 11:33:28 -0500

    reset password without notification

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,8 @@
 // Uncomment these imports to begin using these cool features!
 
 import {repository} from '@loopback/repository';
+import {HttpErrors, post, requestBody} from '@loopback/rest';
 import {UsuarioRepository} from '../repositories';
-import {post, HttpErrors, requestBody} from '@loopback/rest';
 import {AuthService} from '../services/auth.service';
 
 // import {inject} from '@loopback/core';
@@ -11,6 +11,11 @@ import {AuthService} from '../services/auth.service';
 class Credentials {
   correo: string;
   password: string;
+}
+
+class PasswordResetData {
+  correo: string;
+  type: number;
 }
 
 export class UserController {
@@ -45,8 +50,40 @@ export class UserController {
     } else {
       throw new HttpErrors[401]("User or Password invalid.");
     }
-
   }
 
+  @post('/contrasena-reset', {
+    responses: {
+      '200': {
+        description: 'Login para usuarios'
+      }
+    }
+
+  })
+  async reset(
+    @requestBody() passwordResetData: PasswordResetData
+  ): Promise<boolean> {
+    let randomPassword = await this.authService.ResetPassword(passwordResetData.correo);
+    //1.SMS
+    //2.mail
+    if (randomPassword) {
+      switch (passwordResetData.type) {
+        case 1:
+          console.log("sending SMS" + randomPassword);
+          return true;
+          break;
+        case 2:
+          console.log("sending mail" + randomPassword);
+          return true;
+          break;
+
+        default:
+          throw new HttpErrors[400]("this notification type is not supported");
+          break;
+      }
+    }
+    throw new HttpErrors[400]("user not found");
+
+  }
 }
 
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,6 @@
 import {repository} from '@loopback/repository';
+import {generate as generator} from 'generate-password';
+import {PasswordKeys as passKeys} from '../keys/password-keys';
 import {ServiceKeys as keys} from '../keys/services-keys';
 import {Usuario} from '../models';
 import {UsuarioRepository} from '../repositories';
@@ -13,6 +15,12 @@ export class AuthService {
 
   }
 
+  /**
+   *
+   * @param correo
+   * @param password
+   */
+
   async Identify(correo: string, password: string): Promise<Usuario | false> {
     let user = await this.userRepository.findOne({where: {correo: correo}});
     if (user) {
@@ -24,28 +32,59 @@ export class AuthService {
     return false;
   }
 
-  async GenerateToken(user:Usuario){
+  /**
+   *
+   * @param user
+   */
+
+  async GenerateToken(user: Usuario) {
     user.contrasena = '';
     let token = jwt.sign({
       exp: keys.TOKEN_EXPIRATION_TIME,
-      data:{
+      data: {
         _id: user.id,
         username: user.correo,
         edad: user.edad,
         perfilId: user.perfilId
       }
     },
-    keys.JWT_SECRET_KEY );
+      keys.JWT_SECRET_KEY);
     return token;
   }
 
-  async VerifyToken(token: string){
-    try{
+  /**
+   *
+   * @param token
+   */
+
+  async VerifyToken(token: string) {
+    try {
       let data = jwt.verify(token, keys.JWT_SECRET_KEY);
       return data;
-    }catch (error){
+    } catch (error) {
       return false;
     }
   }
+  /**
+   * reset user password
+   * @param correo
+   */
+  async ResetPassword(correo: string): Promise<string | false> {
+    let usuario = await this.userRepository.findOne({where: {correo: correo}});
+    if (usuario) {
+      let randomPassword = generator({
+        length: passKeys.LENGTH,
+        numbers: passKeys.NUMBERS,
+        lowercase: passKeys.LOWERCASE,
+        uppercase: passKeys.UPPERCASE
+      });
+      let crypter = new EncryptDecrypt(keys.LOGIN_CRYPT);
+      let password = crypter.Encrypt(crypter.Encrypt(randomPassword));
+      usuario.contrasena = password;
+      this.userRepository.replaceById(usuario.id, usuario);
+      return randomPassword;
+    }
+    return false;
 
+  }
 }
